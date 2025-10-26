b6e9630fbc063f2b21caab16de483688a8fde078
candle17733883 <marzoukinou40@gmail.com>
2025-10-26 18:51:50 +0100

    fixed double notes

--- app/src/main/java/com/tile/tuoluoyi/GamePadNative.java
+++ app/src/main/java/com/tile/tuoluoyi/GamePadNative.java
@@ -171,15 +171,13 @@ public class GamePadNative {
                     // When in qwerty hold,  isDown = false will be ignored(since it'll unhold immediately as no games properly utilize it)
                     // When in MidiConnect(Piano Rooms) mode, isDown=false will be taken into consideration
 
-//                    status=nativePianoKey(noteNumber, isDown);
-                    nativeQwertyKey(noteNumber, isDown);
 
-//                    if (isDown) {
-//                        //Log.d(MainActivity.TAG, "Key: " + Hid.keyboardCode[2])
-//                        status=nativePianoKey(noteNumber, isDown);
-//                    } else {
-//                        status=nativePianoKey(noteNumber, isDown);
-//                    }
+                      if (isDown) {
+                          //Log.d(MainActivity.TAG, "Key: " + Hid.keyboardCode[2])
+                          status=nativePianoKey(noteNumber, isDown);
+                      } else {
+                          status=nativePianoKey(noteNumber, isDown);
+                      }
 
 
 
